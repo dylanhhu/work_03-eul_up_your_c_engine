77c597c166b71446e813350ab23cb5eb8a25e385
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2021-09-22 11:49:42 -0400

    finished first problem

--- work03.c
+++ work03.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+int problem_1() {
+	int tally = 0;
+
+	int i = 0;
+	for (i; i < 1000; i++) {
+		if (i % 5 == 0 || i % 3 == 0) {
+			tally += i;
+		}
+	}
+
+	return tally;
+}
+
 int main() {
+	int p1_result = problem_1();
+	printf("Problem 1: %d", p1_result);
+
+	//
+
+	printf("\n");
 	return 0;
 }

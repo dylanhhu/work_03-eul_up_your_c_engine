603d7dd1f0278546c5109f79de59367d0d205442
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2021-09-22 21:35:45 -0400

    finished problem 6

--- work03.c
+++ work03.c
@@ -15,23 +15,21 @@ int problem_1() {
 
 int problem_6() {
 	int sum_squares = 0;
-	int i = 0;
-	for (i; i < 100; i++) {
+	int i = 1;
+	for (i; i <= 100; i++) {
 		sum_squares += i * i;
 	}
 
-	int squared_sum = (100 * (101)) / 2;
+	int squared_sum = (100 * 101) / 2;
 	squared_sum *= squared_sum;
 
 	return squared_sum - sum_squares;
 }
 
 int main() {
-	int p1_result = problem_1();
-	printf("Problem 1: %d", p1_result);
+	printf("Problem 1: %d\n", problem_1());
 
-	int p6_result = problem_6();
-	printf("Problem 6: %d", p6_result);
+	printf("Problem 6: %d\n", problem_6());
 
 	printf("\n");
 	return 0;
